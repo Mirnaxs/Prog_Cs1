6bf90316991be79d7f89bf225ade4913bca40e91
Marnix <marnixvanderwerff@gmail.com>
2025-10-27 10:18:10 +0100

    nieuw

---
+++ Opdrachten/06_loops/basicloop/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace basicloop;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nummers = new int[] { 42, 60, 89 };
+
+        for (int i = 0; i < nummers.Length; i++)
+        {
+            int nummer = nummers[i];
+        }
+    }
+}
+
+
