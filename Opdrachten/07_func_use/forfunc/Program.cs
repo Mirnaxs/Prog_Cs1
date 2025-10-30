b0d33be36a31523040a94932cef75c510c881b44
Marnix <marnixvanderwerff@gmail.com>
2025-10-30 12:51:18 +0100

    Nieuw

---
+++ Opdrachten/07_func_use/forfunc/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace forfunc;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] basis = { 1, 5, 7, 10 };
+
+        for(int i =0; i< basis.Length; i++)
+        {
+            Console.WriteLine(basis[i] + " keer zichzelf " + Math.Pow(basis[i],basis[i]));
+        }
+    }
+}
