acb929fd23b1b39b21e41c734992df51eafbb418
Marnix <marnixvanderwerff@gmail.com>
2025-10-30 12:38:02 +0100

    Nieuwe opdrachtjes toevoegen

---
+++ Opdrachten/07_func_use/funccall/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _07_func_use;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.Clear();
+        Console.Write("....X....");
+        Console.SetCursorPosition(4, 0);
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write("?");
+        Console.ResetColor();
+        Console.WriteLine("\nX was nothing");
+    }
+}
---
+++ Opdrachten/07_func_use/math/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace math;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        double kleiner = 9;
+        double groter = 19;
+
+        double welkeIsKleiner = Math.Min(kleiner, groter);
+        double welkeIsGroter = Math.Max(kleiner, groter);
+        double squareRoot1 = Math.Sqrt(kleiner);
+        double squareRoot2 = Math.Sqrt(groter);
+        double afgerond = Math.Round(squareRoot2);
+
+        Console.WriteLine($"\nDe kleinste waarde van {kleiner} en {groter} is {welkeIsKleiner}");
+        Console.WriteLine($"{welkeIsGroter} is groter dan {welkeIsKleiner}");
+        Console.WriteLine($"De wortel van {kleiner} is {squareRoot1}");
+        Console.WriteLine($"Als je de wortel van {groter} afrond is het {afgerond}");
+    }
+}
