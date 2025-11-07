7d885c511372bf80e0b1addfbfdcd2e79436ae1d
Marnix <marnixvanderwerff@gmail.com>
2025-11-07 11:24:11 +0100

    nieuw

---
+++ Opdrachten/08_class_intro/autoclass/Auto.cs
@@ -0,0 +1,18 @@
+﻿namespace autoclass;
+
+class Auto
+{
+    internal string merk;
+    internal int bouwjaar;
+
+    static void Main(string[] args)
+    {
+        Auto mijnAuto = new Auto();
+        mijnAuto.merk = "Ford";
+        Console.WriteLine(mijnAuto);
+
+        Console.WriteLine("Auto merk: " + mijnAuto.merk);
+    }
+
+};
+
